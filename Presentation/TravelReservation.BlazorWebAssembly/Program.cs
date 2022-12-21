1ded121dc97358012eb88a75bf3199226c58765e
Musa UYUMAZ <113431738+musauyumaznotes@users.noreply.github.com>
2022-12-21 22:23:58 +0300

    Proje Template'i eklendi ve düzenlemeler gerçekleştirildi

---
+++ Presentation/TravelReservation.BlazorWebAssembly/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using TravelReservation.BlazorWebAssembly;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
+//builder.Services.AddAntDesign();
+builder.Services.AddMudServices();
